c7653b71b00df0385376f1b59b078df521289ede
tojiboyeva1marjona <tojiboyevajumagul94@gmail.com>
2022-03-08 13:16:43 +0500

    videodagi xatolar tuzatildi

--- src/containers/video/video-edit/index.jsx
+++ src/containers/video/video-edit/index.jsx
@@ -1,7 +1,110 @@
-import React from "react";
+
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
+import { Button, Form } from "react-bootstrap";
 
 const VideoEdit = () => {
-  return <div>VideoEdit</div>;
+  const { state } = useLocation();
+  const navigate = useNavigate();
+
+  const [VideoURL, setVideoURL] = useState("");
+  const [ImageURL, setImageURL] = useState(undefined);
+  const [Title, setTitle] = useState("");
+  const [Description, setDescription] = useState("");
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/api/videos/${state}`).then((res) => {
+      setVideoURL(res.data.Url);
+      setImageURL(res.data.img_url);
+      setTitle(res.data.title);
+      setDescription(res.data.text);
+    });
+  }, [state]);
+
+  const Submit = (e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("text", Description);
+    formData.append("file", ImageURL);
+    formData.append("title", Title);
+    formData.append("Url", VideoURL);
+    
+
+    var confirm = window.confirm("Are you sure?")
+      if (confirm==true) {
+        axios
+      .put(`http://localhost:8080/api/videos/${state}`, formData)
+      .then((resp) => {
+        alert("Product successfully Updated!!!");
+        navigate("/video");
+        setTitle("");
+        setImageURL("");
+        setDescription("");
+        setVideoURL("");
+      })
+      } else{
+        navigate("/video");
+      }
+  };
+
+  return (
+    <div>
+      <div className="video_view my-5 mx-5 px-3 py-5">
+        <div>
+          <h3>Video Tahrirlash</h3>
+        </div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Video URL</Form.Label>
+            <Form.Control
+              onChange={(e) => setVideoURL(e.target.value)}
+              value={VideoURL}
+              type="text"
+              placeholder="Enter the URL of video"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Image URL</Form.Label>
+            <Form.Control
+              onChange={(e) => setImageURL(e.target.files[0])}
+              // value={ImageURL}
+              type="file"
+              placeholder="Enter the URL of image"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Video Title</Form.Label>
+            <Form.Control
+              onChange={(e) => setTitle(e.target.value)}
+              value={Title}
+              type="text"
+              placeholder="Enter the title"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Video Description</Form.Label>
+            <Form.Control
+              onChange={(e) => setDescription(e.target.value)}
+              value={Description}
+              type="text"
+              placeholder="Enter the description"
+            />
+          </Form.Group>
+
+          <Button
+            style={{ backgroundColor: "#3c4b64", border: "#3c4b64" }}
+            onClick={Submit}
+          >
+            Edit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default VideoEdit;
--- src/containers/video/video-view/index.jsx
+++ src/containers/video/video-view/index.jsx
@@ -1,7 +1,117 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button, Table } from "react-bootstrap";
+import { FaBan, FaEdit } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./index.css";
 
 const VdeoView = () => {
-  return <div>VdeoView</div>;
+  const [videoData, setVideoData] = useState([]);
+  const navigate = useNavigate();
+
+  const handleNavigate = (id) => {
+    var confirm= window.confirm("Are you sure?")
+      if (confirm==true) {
+        navigate("/video/edit", { state: id });
+      }
+    
+  };
+
+  useEffect(() => {
+    fetchVideo();
+  }, []);
+
+  const fetchVideo = () => {
+    axios.get("http://localhost:8080/api/videos").then((resp) => {
+      const filteredData = resp.data.map((item) => ({
+        id: item.id,
+        title: item.title,
+        img_url: item.img_url,
+        text: item.text,
+        url: item.Url,
+      }));
+
+      setVideoData(filteredData);
+    });
+  };
+  const Delete = (id) => {
+    var ask = window.confirm("Are you sure?");
+    if (ask == true) {
+      axios
+        .delete(`http://localhost:8080/api/videos/${id}`)
+        .then((res) => {
+          fetchVideo();
+        })
+        
+    }
+  };
+
+  return (
+    <div className="video_view my-5 mx-5 px-3">
+      <div className="d-flex gap-5 my-5 mx-5">
+        <div className="video_text">
+          <h4>Video</h4>
+        </div>
+        <div className="video_add_btn">
+          <Link to="/video/add">
+            <Button style={{ backgroundColor: "green" }} size="lg" active>
+              Add
+            </Button>
+          </Link>
+        </div>
+        <div></div>
+      </div>
+
+      <div>
+        <Table className="video_table" striped bordered hover>
+          <thead>
+            <tr className="table_header">
+              <th>T/r</th>
+              <th>Video</th>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {videoData.map((data, key) => (
+              <tr key={key}>
+                <td>{data.id}</td>
+                <td>
+                  <video style={{ maxWidth: 120, height: 100 }} controls>
+                    <source src={data.Url} type="video" />
+                    Your browser does not support the html video tag.
+                  </video>
+                </td>
+                <td>
+                  <img
+                    style={{ width: 100, height: 100 }}
+                    src={data.img_url}
+                    alt=""
+                  />
+                </td>
+                <td>{data.title}</td>
+                <td>{data.text}</td>
+                <td className="d-flex">
+                  <div
+                    onClick={() => handleNavigate(data.id)}
+                    className="edit_icon"
+                  >
+                    <FaEdit />
+                  </div>
+
+                  <div onClick={() => Delete(data.id)} className="del_icon">
+                    <FaBan />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default VdeoView;
